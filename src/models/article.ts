feefdf98e3151688dd3bd34ff9d79be8a590f794
King Elisha <ek.chibueze@gmail.com>
2020-05-02 17:15:11 +0100

    added sequelize model

--- src/models/article.ts
+++ src/models/article.ts
@@ -6,3 +6,36 @@ export interface Article {
   source: string;
   date: number;
 }
+
+import Sequelize from "sequelize";
+import { db } from "../config/db";
+
+const Article = db.define("article", {
+  url: {
+    allowNull: false,
+    type: Sequelize.STRING,
+  },
+  imageURL: {
+    allowNull: false,
+    type: Sequelize.STRING,
+  },
+  title: {
+    allowNull: false,
+    type: Sequelize.STRING,
+  },
+
+  description: {
+    allowNull: false,
+    type: Sequelize.STRING,
+  },
+  source: {
+    allowNull: false,
+    type: Sequelize.STRING,
+  },
+  date: {
+    allowNull: false,
+    type: Sequelize.INTEGER,
+  },
+});
+
+Article.sync();
