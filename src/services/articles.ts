53bbdb6754527cae8f49e5a718e2166ae108977d
King Elisha <ek.chibueze@gmail.com>
2020-05-05 14:31:14 +0100

    getArticleByID service

--- src/services/articles.ts
+++ src/services/articles.ts
@@ -16,3 +16,23 @@ export function getArticles(): Promise<ArticleModelAttributes[]> {
       });
   });
 }
+
+/**
+ * Finds and returns an `article` with the specified `id` from the database
+ */
+export function getArticleByID(id: number): Promise<ArticleModelAttributes> {
+  return new Promise((resolve, reject) => {
+    ArticleModel.findByPk(id)
+      .then((article) => {
+        if (article) resolve(article);
+        else {
+          logError(`Article with id: ${id} does not exist`);
+          reject(`Article with id: ${id} does not exist`);
+        }
+      })
+      .catch((err) => {
+        logError(err);
+        reject(err);
+      });
+  });
+}
