47267946b1fd8e1b70237fe794874d49bf5e3bbf
King Elisha <ek.chibueze@gmail.com>
2020-05-06 13:25:34 +0100

    enable cors and format console.log

--- src/server.ts
+++ src/server.ts
@@ -4,24 +4,39 @@ import { join, dirname } from "path";
 dotenv.config({ path: join(dirname(__dirname), ".env") });
 
 import express from "express";
+import cors from "cors";
 
 import { articles } from "./routes/articles";
 import { runJobs } from "./jobs";
 import { db } from "./config/db";
 
 const server = express();
-
-server.get("/", (req, res) => {
-  res.send("Updates on <strong>Space Exploration</strong>");
-});
-
-server.use("/api/v1/articles", articles);
+server.use(cors());
 
 db.authenticate()
   .then(() => {
     console.log("DB connected");
   })
   .catch((err) => console.log("Error: " + err));
+
+server.all("*", (req, res, next) => {
+  const date = new Date();
+  const timeString = `${date.toDateString()} ${date
+    .toTimeString()
+    .split("(")[0]
+    .trim()}`;
+  console.log(
+    "\x1b[32m%s\x1b[0m",
+    `${timeString} - ${req.method} request from: ${req.originalUrl}`
+  );
+  next();
+});
+
+server.get("/", (req, res) => {
+  res.send("Updates on <strong>Space Exploration</strong>");
+});
+
+server.use("/api/v1/articles", articles);
 
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
